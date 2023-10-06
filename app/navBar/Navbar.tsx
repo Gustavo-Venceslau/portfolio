607470c04928d21db44ad593ecbbc3d7d2b489d5
Gustavo-de-Almeida <gustavodealmeida01@gmail.com>
2023-10-05 21:45:33 -0300

    smooth scrolling added

--- app/navBar/Navbar.tsx
+++ app/navBar/Navbar.tsx
@@ -1,14 +1,18 @@
-import Link from "next/link";
 import { Logo } from "../utils/logo";
 import { SectionLinks } from "../utils/sectionLinks";
 
 export function Navbar(){
 
     return(
-        <nav className="w-full h-20 flex justify-between items-center px-20">
+        <nav className="w-full h-20 flex justify-between items-center px-20" id="Home">
             <Logo />
             <SectionLinks />
-            <button className="bg-[#2b4c7e] hover:bg-[#2b4c7e]/75 shadow-xl w-36 h-10 rounded-full text-white font-semibold">Talk to me</button>
+            <a 
+				className="bg-[#2b4c7e] hover:bg-[#2b4c7e]/75 shadow-xl w-36 h-10 rounded-full text-white font-semibold flex items-center justify-center cursor-pointer"
+				href="#TalkToMe"
+			>
+				Talk to me
+			</a>
         </nav>
     )
 }
--- app/pageContent/Page.tsx
+++ app/pageContent/Page.tsx
@@ -5,7 +5,7 @@ import { LeftSideContent } from "./components/LeftSideContent";
 
 export function PageContent(){
     return(
-        <div className="px-20 w-full h-[90vh]" id="Home">
+        <div className="px-20 w-full h-[90vh]">
             <section className="w-full flex flex-row">
                 <RightSideContent />
                 <LeftSideContent />
